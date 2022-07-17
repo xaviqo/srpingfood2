ff3a96a7fa1f51af6e656fc6db245cb25bc9bb0d
xaviqo <silent90s@gmail.com>
2022-07-17 10:27:51 +0200

    First commit with the Spring Template

---
+++ src/test/java/xavi/tech/springfood/SpringFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package xavi.tech.springfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
